6a934d2529818c078838cfcd384f4a76a07a1fa1
Brage Allum <brageallum@gmail.com>
2019-04-07 22:48:18 +0200

    Made a component out of the editor page.

--- src/main/java/fa/components/Editor.java
+++ src/main/java/fa/components/Editor.java
@@ -1,30 +1,44 @@
-package fa;
+package fa.components;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 
 import fa.models.DB;
-import fa.models.Jobseeker;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
-/**
- * This will be used as an abstraction for the data specific editors
- * Todo: Implement data specific editors
- */
-public class EditorController {
+public class Editor extends SplitPane {
   @FXML private ListView<String> itemsList;
-  @FXML private Label displayData;
+  @FXML private Pane editorFormContainer;
+
+  public Editor() {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fa/components/Editor.fxml"));
+    fxmlLoader.setRoot(this);
+    fxmlLoader.setController(this);
+
+    try {
+      fxmlLoader.load();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public ObservableList<Node> getForm() {
+    return editorFormContainer.getChildren();
+  }
 
   public void initialize() {
-    System.out.format("[ %s ]: EditorController initialized.\n", new Date());
+    System.out.format("[ %s ]: Editor initialized.\n", new Date());
 
     ArrayList<String> jobkeeperName = new ArrayList<>();
     DB.init().getJobseekers().forEach((k) -> {
@@ -37,8 +51,9 @@ public class EditorController {
     itemsList.setCellFactory(stringListView -> new DataListCell());
     itemsList.setItems(items);
 
+    // TODO: This needs to be changed to pass the new value to the editor form
     itemsList.getSelectionModel().selectedItemProperty().addListener(
-      (observableValue, oldValue, newValue) -> displayData.setText(newValue)
+      (observableValue, oldValue, newValue) -> ((Label)editorFormContainer.getChildren().get(0)).setText(newValue)
     );
   }
 }
