f5cd519ee60f0a395bf8796cfc82a360808a7b5e
Brage Allum <brageallum@gmail.com>
2019-04-09 10:19:21 +0200

    Set up the basic structure for the JobSeekerEditor (the editor can now edit the first name and last name fields).

--- src/main/java/fa/components/JobSeekerEditor.java
+++ src/main/java/fa/components/JobSeekerEditor.java
@@ -12,17 +12,59 @@ public class JobSeekerEditor {
   @FXML private Label title;
   @FXML private Editor<JobSeeker> editor;
   @FXML private TextField firstNameField;
+  @FXML private TextField lastNameField;
+
+  private JobSeeker selectedItem;
 
   public void initialize() {
     System.out.format("[ %s ]: JobSeekerEditor initialized.\n", new Date());
 
     editor.setItemsList(DB.init().getJobSeekers());
-    editor.onNewItem((observableValue, oldValue, newValue) -> title.setText("Editing: " + newValue.toString()));
+    editor.onNewItem((observableValue, oldValue, newValue) -> selectItem(newValue));
+  }
+
+  private void selectItem(JobSeeker jobSeeker) {
+    title.setText("Editing: " + jobSeeker.toString());
+    selectedItem = jobSeeker;
+
+    firstNameField.setText(jobSeeker.getFirstName().getValue());
+    lastNameField.setText(jobSeeker.getLastName().getValue());
   }
 
   @FXML
-  public void firstnameHandler() {
-    System.out.println("You typed: " + firstNameField.getText());
-    DB.init().getJobSeekers().get(0).getFirstName().set(firstNameField.getText());
+  public void submit() {
+    if (selectedItem == null) {
+      createNewJobSeeker();
+    } else {
+      updateJobSeeker();
+    }
+    clearForm();
+  }
+
+  private void createNewJobSeeker() {
+    DB.init().getJobSeekers().add(new JobSeeker(
+      firstNameField.getText(),
+      lastNameField.getText(),
+      "",
+      "",
+      new Date(),
+      "",
+      "",
+      2,
+      ""
+    ));
+  }
+
+  private void updateJobSeeker() {
+    selectedItem.getFirstName().set(firstNameField.getText());
+    selectedItem.getLastName().set(lastNameField.getText());
+  }
+
+  private void clearForm() {
+    title.setText("");
+    selectedItem = null;
+
+    firstNameField.setText("");
+    lastNameField.setText("");
   }
 }
